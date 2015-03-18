bb9a359fa09747f3517ff30935cadcf64aa23eae
paulyang1990 <paulyang1990@gmail.com>
2015-03-18 08:43:50 +0800

    Initial Commit

---
+++ MyTriangulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyTriangulation
+//
+//  Created by Yang Shuo on 18/3/15.
+//  Copyright (c) 2015 Yang Shuo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
